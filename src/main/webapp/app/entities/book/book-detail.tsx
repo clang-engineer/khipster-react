3768854624a2f5834016b81da6eb7dcb70d5b69d
yorez <yorez@naver.com>
2023-02-17 10:44:04 +0900

    step3: book - react

---
+++ src/main/webapp/app/entities/book/book-detail.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Button, Row, Col } from 'reactstrap';
+import { Translate } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+import { useAppDispatch, useAppSelector } from 'app/config/store';
+
+import { getEntity } from './book.reducer';
+
+export const BookDetail = () => {
+  const dispatch = useAppDispatch();
+
+  const { id } = useParams<'id'>();
+
+  useEffect(() => {
+    dispatch(getEntity(id));
+  }, []);
+
+  const bookEntity = useAppSelector(state => state.book.entity);
+  return (
+    <Row>
+      <Col md="8">
+        <h2 data-cy="bookDetailsHeading">
+          <Translate contentKey="specialistSchedulerApp.book.detail.title">Book</Translate>
+        </h2>
+        <dl className="jh-entity-details">
+          <dt>
+            <span id="id">
+              <Translate contentKey="global.field.id">ID</Translate>
+            </span>
+          </dt>
+          <dd>{bookEntity.id}</dd>
+          <dt>
+            <span id="title">
+              <Translate contentKey="specialistSchedulerApp.book.title">Title</Translate>
+            </span>
+          </dt>
+          <dd>{bookEntity.title}</dd>
+          <dt>
+            <span id="description">
+              <Translate contentKey="specialistSchedulerApp.book.description">Description</Translate>
+            </span>
+          </dt>
+          <dd>{bookEntity.description}</dd>
+        </dl>
+        <Button tag={Link} to="/book" replace color="info" data-cy="entityDetailsBackButton">
+          <FontAwesomeIcon icon="arrow-left" />{' '}
+          <span className="d-none d-md-inline">
+            <Translate contentKey="entity.action.back">Back</Translate>
+          </span>
+        </Button>
+        &nbsp;
+        <Button tag={Link} to={`/book/${bookEntity.id}/edit`} replace color="primary">
+          <FontAwesomeIcon icon="pencil-alt" />{' '}
+          <span className="d-none d-md-inline">
+            <Translate contentKey="entity.action.edit">Edit</Translate>
+          </span>
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
+export default BookDetail;
---
+++ src/main/webapp/app/entities/book/book-update.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Button, Row, Col, FormText } from 'reactstrap';
+import { isNumber, Translate, translate, ValidatedField, ValidatedForm } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { convertDateTimeFromServer, convertDateTimeToServer, displayDefaultDateTime } from 'app/shared/util/date-utils';
+import { mapIdList } from 'app/shared/util/entity-utils';
+import { useAppDispatch, useAppSelector } from 'app/config/store';
+
+import { IBook } from 'app/shared/model/book.model';
+import { getEntity, updateEntity, createEntity, reset } from './book.reducer';
+
+export const BookUpdate = () => {
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  const { id } = useParams<'id'>();
+  const isNew = id === undefined;
+
+  const bookEntity = useAppSelector(state => state.book.entity);
+  const loading = useAppSelector(state => state.book.loading);
+  const updating = useAppSelector(state => state.book.updating);
+  const updateSuccess = useAppSelector(state => state.book.updateSuccess);
+
+  const handleClose = () => {
+    navigate('/book' + location.search);
+  };
+
+  useEffect(() => {
+    if (isNew) {
+      dispatch(reset());
+    } else {
+      dispatch(getEntity(id));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (updateSuccess) {
+      handleClose();
+    }
+  }, [updateSuccess]);
+
+  const saveEntity = values => {
+    const entity = {
+      ...bookEntity,
+      ...values,
+    };
+
+    if (isNew) {
+      dispatch(createEntity(entity));
+    } else {
+      dispatch(updateEntity(entity));
+    }
+  };
+
+  const defaultValues = () =>
+    isNew
+      ? {}
+      : {
+          ...bookEntity,
+        };
+
+  return (
+    <div>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <h2 id="specialistSchedulerApp.book.home.createOrEditLabel" data-cy="BookCreateUpdateHeading">
+            <Translate contentKey="specialistSchedulerApp.book.home.createOrEditLabel">Create or edit a Book</Translate>
+          </h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <ValidatedForm defaultValues={defaultValues()} onSubmit={saveEntity}>
+              {!isNew ? (
+                <ValidatedField
+                  name="id"
+                  required
+                  readOnly
+                  id="book-id"
+                  label={translate('global.field.id')}
+                  validate={{ required: true }}
+                />
+              ) : null}
+              <ValidatedField
+                label={translate('specialistSchedulerApp.book.title')}
+                id="book-title"
+                name="title"
+                data-cy="title"
+                type="text"
+                validate={{
+                  required: { value: true, message: translate('entity.validation.required') },
+                  minLength: { value: 5, message: translate('entity.validation.minlength', { min: 5 }) },
+                  maxLength: { value: 20, message: translate('entity.validation.maxlength', { max: 20 }) },
+                }}
+              />
+              <ValidatedField
+                label={translate('specialistSchedulerApp.book.description')}
+                id="book-description"
+                name="description"
+                data-cy="description"
+                type="text"
+              />
+              <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to="/book" replace color="info">
+                <FontAwesomeIcon icon="arrow-left" />
+                &nbsp;
+                <span className="d-none d-md-inline">
+                  <Translate contentKey="entity.action.back">Back</Translate>
+                </span>
+              </Button>
+              &nbsp;
+              <Button color="primary" id="save-entity" data-cy="entityCreateSaveButton" type="submit" disabled={updating}>
+                <FontAwesomeIcon icon="save" />
+                &nbsp;
+                <Translate contentKey="entity.action.save">Save</Translate>
+              </Button>
+            </ValidatedForm>
+          )}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default BookUpdate;
